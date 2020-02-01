ba3068af78eb70dd84af2960fd660c8a460110ae
andrewmbrown <35706781+andrewmbrown@users.noreply.github.com>
2020-01-31 22:11:38 -0500

    updated recipe scraper

--- recipescraper.py
+++ recipescraper.py
@@ -11,14 +11,15 @@ success is 200. troubleshoot codes at restapitutorial.com
 
 CONTENT: the content we'd like to grab/print
 '''
-url = 'http://ethans_fake_twitter_site.surge.sh/'
+url = 'https://www.allrecipes.com/recipes/105/appetizers-and-snacks/dips-and-spreads/?internalSource=recipe%20breadcrumb&referringId=14231&referringContentType=Recipe&referringPosition=1&clickId=recipe%20breadcrumb%204/'
 response = requests.get(url, timeout=5)  # access url 5 times max
 content = BeautifulSoup(response.content, "html.parser")
-tweetArr = []
-# print(content)
+recipes = []
+print(content)
 # step = content.find('div', attrs={"class": "direction-lists"}).text
 # print(step)
 # creating json objects with KEY: VALUE
+'''
 for tweet in content.findAll('div', attrs={"class": "tweetcontainer"}):
     tweetObject = {
         "author": tweet.find('h2', attrs={"class": "author"}).text,
@@ -37,4 +38,4 @@ with open('twitterData.json', 'w') as outfile:
 
 # select all <p> paragraph tags with tag content .text(only text
 # contents of the file)
-
+'''
