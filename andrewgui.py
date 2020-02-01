33f296fc5052ed2cedfd3ece0d9f3b60ec6e52d3
andrewmbrown <35706781+andrewmbrown@users.noreply.github.com>
2020-02-01 15:58:15 -0500

    andrewgui.py accepts entry text which should be fed to recipe scraper

--- andrewgui.py
+++ andrewgui.py
@@ -8,10 +8,9 @@ SMALL_FONT = ("Verdana", 8)
 
 class window(tk.Tk):
     def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
-        tk.Tk.__init__(self,*args, **kwargs)
-
-        #tk.Tk.iconbitmap(self)
+        # tk.Tk.iconbitmap(self)
         tk.Tk.wm_title(self, "RecipeScraper.V1.0")
 
         container = tk.Frame(self)
@@ -33,27 +32,33 @@ class window(tk.Tk):
         frame.tkraise()
 
 
-def qf(param):
-    print(param)
-
-
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+
+        def searchRecipe():
+            looking_for = message1.get()
+            print(looking_for)
+
         label = ttk.Label(self, text="What food would you like to make?", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
         message1 = ttk.Entry(self)
         message1.pack()
-        print(message1)
+
+        '''
         button1 = ttk.Button(self, text="Search",
                             command=lambda: controller.show_frame(PageOne))
         button1.pack()
+        '''
+        button1 = ttk.Button(self, text="Search",
+                             command=lambda: [searchRecipe(),
+                                              controller.show_frame(PageOne)])
+        button1.pack()
 
-
-        #answer = simpledialog.askstring("Input", "Insert food item",
+        # answer = simpledialog.askstring("Input", "Insert food item",
         #                                parent=self)
-        #print(answer)
+        # print(answer)
 
 
 class PageOne(tk.Frame):
@@ -62,12 +67,13 @@ class PageOne(tk.Frame):
         label = tk.Label(self, text="Food options displayed here", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
         button2 = ttk.Button(self, text="Back to Search",
-                            command=lambda: controller.show_frame(StartPage))
+                             command=lambda: controller.show_frame(StartPage))
         button2.pack()
 
         button3 = ttk.Button(self, text="To page 2",
-                            command=lambda: controller.show_frame(PageTwo))
+                             command=lambda: controller.show_frame(PageTwo))
         button3.pack()
+
 
 class PageTwo(tk.Frame):
     def __init__(self, parent, controller):
@@ -75,7 +81,7 @@ class PageTwo(tk.Frame):
         label = tk.Label(self, text="Extra Page two", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
         button3 = ttk.Button(self, text="Back to food options",
-                            command=lambda: controller.show_frame(PageOne))
+                             command=lambda: controller.show_frame(PageOne))
         button3.pack()
 
 
