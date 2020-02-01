da3d3516d677d0a8b042e5ddeaa4121e7f23dc15
blnm <45343407+blnm@users.noreply.github.com>
2020-02-01 11:54:10 -0500

    created get_recipes_for_search(), added test cases (which just prints out URLs for now)
    get_recipes_for_search() currently outputs to recipes.json with rating, number of reviews, picture, link, and description.

--- recipescraper.py
+++ recipescraper.py
@@ -3,55 +3,62 @@ import requests
 import json
 import re
 
-'''
-python requests libary has 3 parts 
-URL: The link of the website we are accessing  
 
-RESPONSE: result of a GET request. actually an HTTP status code.
-success is 200. troubleshoot codes at restapitutorial.com
+def get_recipes_for_search(item):
+    #item = "guac"#input("enter in what you would like to cook: ")
+    url = 'https://www.allrecipes.com/search/results/?wt=' + str(item) + '&sort=re'
+    #print(url)
 
-CONTENT: the content we'd like to grab/print
-'''
-item = "guac"#input("enter in what you would like to cook")
-url = 'https://www.allrecipes.com/search/results/?wt=' + str(item) + '&sort=re'
-print(url)
+    response = requests.get(url, timeout=5)  # access url 5 times max
+    content = BeautifulSoup(response.content, "html.parser")
+    recipes = []
 
-response = requests.get(url, timeout=5)  # access url 5 times max
-content = BeautifulSoup(response.content, "html.parser")
-recipes = []
-#print(content.prettify())
-#step = content.find('div', attrs={"class": "toggle-similar__title"})
-#link = content.find('a').get('href')
-#print(step)
-#print(link)
-# step = content.find('div', attrs={"class": "direction-lists"}).text
-# print(step)
-# creating json objects with KEY: VALUE
-link = content.find('a').get('href')
-print(link)
+    #link = content.find('a').get('href')
+    #print(link)
 
-print("ratings")
-for recipe in content.findAll('span', attrs={'class': re.compile("^stars stars-.*")}):  #stars stars-5 is the 5 stars image heading. This looks for all ratings
-    print(recipe.attrs['data-ratingstars'])
-    #recipeObject = {
-    #    "rating": recipe.attrs['data-ratingstars']
-    #}
-    #recipes.append(recipeObject)
+    #print("ratings")
+    for recipe in content.findAll('span', attrs={'class': re.compile("^stars stars-.*")}):  #stars stars-5 is the 5 stars image heading. This looks for all ratings
+        recipes.append({"rating": recipe.attrs["data-ratingstars"]})
+        #print(recipe.attrs['data-ratingstars'])
+
+    #print("num ratings")
+    count = 0
+    for num in content.findAll("span", attrs={'class': "fixed-recipe-card__reviews"}):
+        recipes[count]["num_reviews"] = num.contents[0].attrs['number']
+        count += 1
+        #print(num.contents[0].attrs['number'])
+
+    #print("picture, description")
+    count = 0
+    for num in content.findAll("img", attrs={'class': "fixed-recipe-card__img"}):
+        recipes[count]["img"] = num.attrs['data-original-src']
+        recipes[count]["desc"] = num.attrs['alt']
+        count += 1
+        #print(num.attrs['data-original-src'])
+        #print(num.attrs['alt'])
+
+    #print("links")
+    count = 0
+    for recipe in content.findAll('h3', attrs = {"class": "fixed-recipe-card__h3"}):
+        recipes[count]["link"] = recipe.contents[1].attrs['href']
+        count += 1
+        #print(recipe.contents[1].attrs["href"])
 
 
-print("links")
-for recipe in content.findAll('h3', attrs = {"class": "fixed-recipe-card__h3"}):
-    print(recipe.contents[1].attrs["href"])
+    with open('recipes.json', 'w') as outfile:
+        json.dump(recipes, outfile)
 
+    with open('recipes.json') as json_data:
+        jsonData = json.load(json_data)
+    print("link")
+    for i in jsonData:
+        print(i['link'])
+        #if (i['price'].find("$") == -1): continue
 
-with open('recipes.json', 'w') as outfile:
-    json.dump(recipes, outfile)
-
-# select all <p> paragraph tags with tag content .text(only text
-# contents of the file)
-#with open('recipes.json') as json_data:
-#    jsonData = json.load(json_data)
-#print("ratings")
-#for i in jsonData:
-#    print(i['rating'])
-#    #if (i['price'].find("$") == -1): continue
+# TESTING CODE
+print("guac")
+get_recipes_for_search("guac")
+print("mexican")
+get_recipes_for_search("mexican")
+print("dutch oven apple crisp")
+get_recipes_for_search("dutch oven apple crisp")
