9ac8ba5f31c3588486e78102ba4b3fa455acdb3e
blnm <45343407+blnm@users.noreply.github.com>
2020-02-01 14:23:43 -0500

    added get_directions_for_link()

--- DirectionScraper.py
+++ DirectionScraper.py
@@ -2,18 +2,22 @@ from bs4 import BeautifulSoup  # bs4 is beautiful soup
 import requests
 import json
 
-url = 'https://www.allrecipes.com/recipe/13721/fruit-salad/?internalSource=previously%20viewed&referringContentType=Homepage'
-print(url)
+#url = 'https://www.allrecipes.com/recipe/13721/fruit-salad/?internalSource=previously%20viewed&referringContentType=Homepage'
+#print(url)
+def get_directions_for_link(url):
 
-response = requests.get(url, timeout=5)  # access url 5 times max
-content = BeautifulSoup(response.content, "html.parser")
-directions = []
+    response = requests.get(url, timeout=5)  # access url 5 times max
+    content = BeautifulSoup(response.content, "html.parser")
+    directions = []
 
-for direction in content.findAll('span', attrs={"class": "recipe-directions__list--item"}):
-    print(direction.text)
+    for direction in content.findAll('span', attrs={"class": "recipe-directions__list--item"}):
+        print(direction.text)
 
-with open('directions.json', 'w') as outfile:
-    json.dump(directions, outfile)
+    with open('directions.json', 'w') as outfile:
+        json.dump(directions, outfile)
 
-with open('directions.json') as json_data:
-    jsonData = json.load(json_data)
+    with open('directions.json') as json_data:
+        jsonData = json.load(json_data)
+
+# TESTING
+get_directions_for_link('https://www.allrecipes.com/recipe/13721/fruit-salad/?internalSource=previously%20viewed&referringContentType=Homepage')
