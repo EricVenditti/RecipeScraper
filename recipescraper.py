df3dc5a5e34209d04548d84f94d2584954e6e341
andrewmbrown <35706781+andrewmbrown@users.noreply.github.com>
2020-01-31 23:55:20 -0500

    recipe scraper with added search function

--- recipescraper.py
+++ recipescraper.py
@@ -11,31 +11,40 @@ success is 200. troubleshoot codes at restapitutorial.com
 
 CONTENT: the content we'd like to grab/print
 '''
-url = 'https://www.allrecipes.com/recipes/105/appetizers-and-snacks/dips-and-spreads/?internalSource=recipe%20breadcrumb&referringId=14231&referringContentType=Recipe&referringPosition=1&clickId=recipe%20breadcrumb%204/'
+item = input("enter in what you would like to cook")
+url = 'https://www.allrecipes.com/search/results/?wt=' + str(item) + '&sort=re'
+print(url)
+
 response = requests.get(url, timeout=5)  # access url 5 times max
 content = BeautifulSoup(response.content, "html.parser")
 recipes = []
-print(content)
+#print(content.prettify())
+#step = content.find('div', attrs={"class": "toggle-similar__title"})
+#link = content.find('a').get('href')
+#print(step)
+#print(link)
 # step = content.find('div', attrs={"class": "direction-lists"}).text
 # print(step)
 # creating json objects with KEY: VALUE
-'''
-for tweet in content.findAll('div', attrs={"class": "tweetcontainer"}):
-    tweetObject = {
-        "author": tweet.find('h2', attrs={"class": "author"}).text,
-        "date": tweet.find('h5', attrs={"class": "dateTime"}).text,
-        "tweet": tweet.find('p', attrs={"class": "content"}).text
+link = content.find('a').get('href')
+print(link)
+for recipe in content.findAll('article', attrs={"class": "fixed-recipe-card"}):
+    print("hi")
+    recipeObject = {
+        "link": recipe.find('a', attrs={"href"}),
+        #gu"2tag": recipe.find('span', attrs={"itemprop": "name"}).text,
+        #"date": recipe.find('h5', attrs={"class": "dateTime"}).text,
+        #"tweet": recipe.find('p', attrs={"class": "content"}).text
         # "likes": tweet.find('p', attrs={"class": "likes"}),
         # "shares": tweet.find('p', attrs={"class": "shares"})
     }
-    tweetArr.append(tweetObject)
+    recipes.append(recipeObject)
 
 # for tweetObject in tweetArr:
 #	print(tweetObject)
 
-with open('twitterData.json', 'w') as outfile:
-    json.dump(tweetArr, outfile)
+with open('recipes.json', 'w') as outfile:
+    json.dump(recipes, outfile)
 
 # select all <p> paragraph tags with tag content .text(only text
 # contents of the file)
-'''
